9191e82b1418e8e23552cd2232bd214439bbad32
sali1502 <sali1502@sudent.miun.se>
2024-10-02 13:55:23 +0200

    Navigering med RouterLinkActive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { ConvertComponent } from './convert/convert.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'convert', component: ConvertComponent },
-    { path: 'about', component: AboutComponent },
+    { path: 'home', component: HomeComponent, title: "Startsidan" },
+    { path: 'convert', component: ConvertComponent, title: "Konvertering" },
+    { path: 'about', component: AboutComponent, title: "Om uppgiften"},
     { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
