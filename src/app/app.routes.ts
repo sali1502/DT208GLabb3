531b27e1083971fdc0958f4c03ba3b29b7bcb0f1
sali1502 <sali1502@sudent.miun.se>
2024-10-02 10:36:30 +0200

    Ett första commit. Components och routes till undersidor.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConvertComponent } from './convert/convert.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'convert', component: ConvertComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
