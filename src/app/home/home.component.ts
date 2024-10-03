720733053fc71a7c5236b552aacc83836f353dbf
sali1502 <sali1502@sudent.miun.se>
2024-10-03 12:11:45 +0200

    Växkling mellan bilder med knapptryck.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 
+
+// Klass med array, konstruktor och metod för att växla bild vid knapptryck 
 export class HomeComponent {
-  welcomeImage: string = "images/autumnleaf.jpg";
+  images: string[] = [
+    "images/autumnleaf.jpg",
+    "images/rosehip.jpg"
+  ];
+
+  currentImageIndex: number = 0;
+  welcomeImage: string;
+
+  constructor() {
+    this.welcomeImage = this.images[this.currentImageIndex];
+  }
 
   changeImage(): void {
-    this.welcomeImage = "images/rosehip.jpg";
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    this.welcomeImage = this.images[this.currentImageIndex];
   }
 }
